5bb4f7196ae3e8296996c7773104a9d1d2c853c4
Kirill-Tsypushtanov <den270380@gmail.com>
2024-12-23 16:54:38 +0500

    Update matrix.hpp

--- task1/matrix.hpp
+++ task1/matrix.hpp
@@ -1,46 +1,25 @@
 #pragma once
-#include <iostream>
+
 #include <vector>
 #include <stdexcept>
 
-using namespace std;
-
-class Matrix {
+class Matrix 
+{
 public:
-    Matrix() : num_rows(0), num_cols(0) {}
-    explicit Matrix(int rows, int cols);
-
-    Matrix(const Matrix& other) = default;
-    Matrix& operator=(const Matrix& other) = default;
-
-    Matrix(Matrix&& other) noexcept : num_rows(other.num_rows), num_cols(other.num_cols), data(move(other.data)) {
-        other.num_rows = 0;
-        other.num_cols = 0;
-    }
-    Matrix& operator=(Matrix&& other) noexcept {
-        if (this != &other) {
-            num_rows = other.num_rows;
-            num_cols = other.num_cols;
-            data = move(other.data);
-            other.num_rows = 0;
-            other.num_cols = 0;
-        }
-        return *this;
-    }
-
-    void Reset(int rows, int cols);
-    int At(int row, int col) const;
+    Matrix() = default;
+    Matrix(int numRows, int numCols);
+    void Reset(int numRows, int numCols);
     int& At(int row, int col);
+    const int& At(int row, int col) const;
     int GetRows() const;
     int GetCols() const;
 
-    friend istream& operator>>(istream& in, Matrix& matrix);
-    friend ostream& operator<<(ostream& out, const Matrix& matrix);
-    friend bool operator==(const Matrix& lhs, const Matrix& rhs);
-    friend Matrix operator+(const Matrix& lhs, const Matrix& rhs);
+ 
+    bool operator==(const Matrix& m2) const;
+    bool operator!=(const Matrix& m2) const;
+    Matrix operator+(const Matrix& m2) const;
 
 private:
-    int num_rows;
-    int num_cols;
-    vector<vector<int>> data;
-};
+    int rows = 0;
+    int cols = 0;
+    std::vector<std::vector<int>> data;
