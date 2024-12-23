f39cc5b04a965df4b2585b4c8224c6f577aa0cbe
Kirill-Tsypushtanov <den270380@gmail.com>
2024-12-23 16:40:51 +0500

    Update circular_queue.hpp

--- task3/circular_queue.hpp
+++ task3/circular_queue.hpp
@@ -1,8 +1,14 @@
 #pragma once
 
-#include <cstddef>
+#include <vector>
 
 class CircularQueue {
+private: 
+    std::vector<int> buffer;          
+    size_t capacity;        
+    size_t size;          
+    size_t front;         
+    size_t rear;   
 public:
     CircularQueue(size_t size); // создать очередь с определенным размером буффера
     bool Push(int value); // добавить значение в конец очереди (false, если очередь заполнена)
