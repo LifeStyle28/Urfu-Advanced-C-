d821dc7a471ee6e6a1b05132b693ab287a9d90f0
Roman-Mingaleev-RI232702 <roman.mingaleev2001@gmail.com>
2024-12-26 18:58:06 +0500

    Update matrix.cpp

--- task1/matrix.cpp
+++ task1/matrix.cpp
@@ -1,75 +1,65 @@
-#include "matrix.hpp"
+cpp
 #include <stdexcept>
-#include <vector>
 
-// Конструктор с параметрами
-Matrix::Matrix(int numRows, int numCols) {
-    Reset(numRows, numCols);
-}
+class Matrix {
+public:
+    Matrix() = default;
+    Matrix(int numRows, int numCols) : rows_(numRows), cols_(numCols), data_(numRows * numCols, 0) {}
 
-// Метод для сброса размеров матрицы
-void Matrix::Reset(int numRows, int numCols) {
-    if (numRows <= 0 || numCols <= 0) {
-        rows_ = 0;
-        cols_ = 0;
-        data_.clear();
-    } else {
+    void Reset(int numRows, int numCols) {
+        if (numRows <= 0 || numCols <= 0) {
+            rows_ = cols_ = 0;
+            data_.clear();
+            return;
+        }
         rows_ = numRows;
         cols_ = numCols;
-        data_.resize(rows_, std::vector<int>(cols_, 0));
+        data_.resize(numRows * numCols);
     }
-}
-
-// Метод для доступа к элементам матрицы (неконстантный)
-int& Matrix::At(int row, int col) {
-    if (row < 0 || row >= rows_ || col < 0 || col >= cols_) {
-        throw std::out_of_range("Index out of range");
-    }
-    return data_[row][col];
-}
-
-// Метод для доступа к элементам матрицы (константный)
-const int& Matrix::At(int row, int col) const {
-    if (row < 0 || row >= rows_ || col < 0 || col >= cols_) {
-        throw std::out_of_range("Index out of range");
-    }
-    return data_[row][col];
-}
-
-// Метод для получения количества строк
-int Matrix::GetRows() const {
-    return rows_;
-}
-
-// Метод для получения количества столбцов
-int Matrix::GetCols() const {
-    return cols_;
-}
-
-// Оператор сравнения на равенство
-bool Matrix::operator==(const Matrix& m2) {
-    if (rows_ != m2.rows_ || cols_ != m2.cols_) {
-        return false;
-    }
-    return data_ == m2.data_;
-}
-
-// Оператор сравнения на неравенство
-bool Matrix::operator!=(const Matrix& m2) {
-    return !(*this == m2);
-}
-
-// Оператор сложения матриц
-Matrix Matrix::operator+(const Matrix& m2) {
-    if (rows_ != m2.rows_ || cols_ != m2.cols_) {
-        throw std::invalid_argument("Matrices must have the same dimensions for addition");
-    }
-
-    Matrix result(rows_, cols_);
-    for (int i = 0; i < rows_; ++i) {
-        for (int j = 0; j < cols_; ++j) {
-            result.At(i, j) = At(i, j) + m2.At(i, j);
+    int&amp; At(int row, int col) {
+        if (row < 0 || row >= rows_ || col < 0 || col >= cols_) {
+            throw std::out_of_range("Matrix index out of range");
         }
+        return data_[row * cols_ + col];
     }
-    return result;
-}
+    const int&amp; At(int row, int col) const {
+        if (row < 0 || row >= rows_ || col < 0 || col >= cols_) {
+            throw std::out_of_range("Matrix index out of range");
+        }
+        return data_[row * cols_ + col];
+    }
+    int GetRows() const { return rows_; }
+    int GetCols() const { return cols_; }
+
+    bool operator==(const Matrix&amp; m2) {
+        if (rows_ != m2.rows_ || cols_ != m2.cols_) {
+            return false;
+        }
+        for (int i = 0; i < rows_; ++i) {
+            for (int j = 0; j < cols_; ++j) {
+                if (At(i, j) != m2.At(i, j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    bool operator!=(const Matrix&amp; m2) { return !(*this == m2); }
+    Matrix operator+(const Matrix&amp; m2) {
+        if (rows_ != m2.rows_ || cols_ != m2.cols_) {
+            throw std::invalid_argument("Matrix dimensions must match");
+        }
+        Matrix result(rows_, cols_);
+        for (int i = 0; i < rows_; ++i) {
+            for (int j = 0; j < cols_; ++j) {
+                result.At(i, j) = At(i, j) + m2.At(i, j);
+            }
+        }
+        return result;
+    }
+
+private:
+    int rows_ = 0;
+    int cols_ = 0;
+    std::vector<int> data_;
+};
