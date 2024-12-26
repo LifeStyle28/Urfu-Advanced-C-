e40a09892ba2455643870804787acb7bdbcf1245
Paulina Karyagina <115936003+dvesele@users.noreply.github.com>
2024-12-26 17:49:24 +0500

    Update figures.hpp

--- task2/figures.hpp
+++ task2/figures.hpp
@@ -1,46 +1,65 @@
 #pragma once
-#include <iostream>
-#include <vector>
+#include <memory>
 #include <stdexcept>
 
-class Matrix {
+static constexpr double PI = 3.14;
+
+enum class FigureType {
+    TRIANGLE,
+    CIRCLE,
+    RECTANGLE,
+};
+
+class Figure {
 public:
-    Matrix() : num_rows(0), num_cols(0) {}
-    explicit Matrix(int rows, int cols);
-    
-    Matrix(const Matrix& other) = default;
-    Matrix& operator=(const Matrix& other) = default;
-    
-    Matrix(Matrix&& other) noexcept : num_rows(other.num_rows), num_cols(other.num_cols), data(std::move(other.data)) {
-        other.num_rows = 0;
-        other.num_cols = 0;
-    }
-    Matrix& operator=(Matrix&& other) noexcept {
-        if (this != &other) {
-            num_rows = other.num_rows;
-            num_cols = other.num_cols;
-            data = std::move(other.data);
-            other.num_rows = 0;
-            other.num_cols = 0;
-        }
-        return *this;
-    }
+    virtual FigureType Type() const = 0;
+    virtual double Perimeter() const = 0;
+    virtual double Area() const = 0;
+    virtual ~Figure() = default;
+};
 
-    void Reset(int rows, int cols);
-    int At(int row, int col) const;
-    int& At(int row, int col);
-    int GetRows() const;
-    int GetCols() const;
+class Rect : public Figure {
+public:
+    Rect(double width, double height);
+    FigureType Type() const override;
+    double Perimeter() const override;
+    double Area() const override;
 
-    friend std::istream& operator>>(std::istream& in, Matrix& matrix);
-    friend std::ostream& operator<<(std::ostream& out, const Matrix& matrix);
-
-    friend bool operator==(const Matrix& lhs, const Matrix& rhs);
-    friend bool operator!=(const Matrix& lhs, const Matrix& rhs);
-
-    friend Matrix operator+(const Matrix& lhs, const Matrix& rhs);
 private:
-    int num_rows;
-    int num_cols;
-    std::vector<std::vector<int>> data;
+    double width;
+    double height;
+};
+
+class Triangle : public Figure {
+public:
+    Triangle(double a, double b, double c);
+    FigureType Type() const override;
+    double Perimeter() const override;
+    double Area() const override;
+
+private:
+    double a, b, c;
+};
+
+class Circle : public Figure {
+public:
+    Circle(double radius);
+    FigureType Type() const override;
+    double Perimeter() const override;
+    double Area() const override;
+
+private:
+    double radius;
+};
+
+std::unique_ptr<Figure> make_figure(FigureType type, double a, double b = 0, double c = 0);
+
+class WrongTriangle : public std::invalid_argument {
+public:
+    WrongTriangle() : std::invalid_argument("") {}
+};
+
+class LessThanZeroParam : public std::invalid_argument {
+public:
+    LessThanZeroParam() : std::invalid_argument("") {}
 };
