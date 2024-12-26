50d355a726846e3b5933e59fb271f890c690ae85
Roman-Mingaleev-RI232702 <roman.mingaleev2001@gmail.com>
2024-12-26 19:00:34 +0500

    Update matrix.cpp

--- task1/matrix.cpp
+++ task1/matrix.cpp
@@ -1,64 +1,48 @@
+#include "matrix.hpp"
+
 #include <stdexcept>
 
-class Matrix {
-public:
-    Matrix() = default;
-    Matrix(int numRows, int numCols) : rows_(numRows), cols_(numCols), data_(numRows * numCols, 0) {}
+Matrix::Matrix(int numRows, int numCols)
+{
+    // your implementation here
+}
 
-    void Reset(int numRows, int numCols) {
-        if (numRows <= 0 || numCols <= 0) {
-            rows_ = cols_ = 0;
-            data_.clear();
-            return;
-        }
-        rows_ = numRows;
-        cols_ = numCols;
-        data_.resize(numRows * numCols);
-    }
-    int&amp; At(int row, int col) {
-        if (row < 0 || row >= rows_ || col < 0 || col >= cols_) {
-            throw std::out_of_range("Matrix index out of range");
-        }
-        return data_[row * cols_ + col];
-    }
-    const int&amp; At(int row, int col) const {
-        if (row < 0 || row >= rows_ || col < 0 || col >= cols_) {
-            throw std::out_of_range("Matrix index out of range");
-        }
-        return data_[row * cols_ + col];
-    }
-    int GetRows() const { return rows_; }
-    int GetCols() const { return cols_; }
+void Matrix::Reset(int numRows, int numCols)
+{
+    // your implementation here
+}
 
-    bool operator==(const Matrix&amp; m2) {
-        if (rows_ != m2.rows_ || cols_ != m2.cols_) {
-            return false;
-        }
-        for (int i = 0; i < rows_; ++i) {
-            for (int j = 0; j < cols_; ++j) {
-                if (At(i, j) != m2.At(i, j)) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-    bool operator!=(const Matrix&amp; m2) { return !(*this == m2); }
-    Matrix operator+(const Matrix&amp; m2) {
-        if (rows_ != m2.rows_ || cols_ != m2.cols_) {
-            throw std::invalid_argument("Matrix dimensions must match");
-        }
-        Matrix result(rows_, cols_);
-        for (int i = 0; i < rows_; ++i) {
-            for (int j = 0; j < cols_; ++j) {
-                result.At(i, j) = At(i, j) + m2.At(i, j);
-            }
-        }
-        return result;
-    }
+int& Matrix::At(int row, int col)
+{
+    // your implementation here
+}
 
-private:
-    int rows_ = 0;
-    int cols_ = 0;
-    std::vector<int> data_;
-};
+const int& Matrix::At(int row, int col) const
+{
+    // your implementation here
+}
+
+int Matrix::GetRows() const
+{
+    // your implementation here
+}
+
+int Matrix::GetCols() const
+{
+    // your implementation here
+}
+
+bool Matrix::operator==(const Matrix& m2)
+{
+    // your implementation here
+}
+
+bool Matrix::operator!=(const Matrix& m2)
+{
+    // your implementation here
+}
+
+Matrix Matrix::operator+(const Matrix& m2)
+{
+    // your implementation here
+}
