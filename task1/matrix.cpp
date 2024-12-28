66dae7d866dd095ebd355ea4aa6bddae74e25ab2
MAXmvp228 <maksimbobylev2005@gmail.com>
2024-12-28 22:29:03 +0500

    Update matrix.cpp

--- task1/matrix.cpp
+++ task1/matrix.cpp
@@ -4,45 +4,114 @@
 
 Matrix::Matrix(int numRows, int numCols)
 {
-    // your implementation here
+    if (numRows < 0 || numCols < 0) {
+        throw out_of_range("");
+    }
+    if (numRows == 0 || numCols == 0) {
+        x = 0;
+        y = 0;
+        Z.clear();
+    }
+    else {
+        x = numRows;
+        y = numCols;
+        Z.assign(x, vector<int>(y, 0));
+    }
 }
 
 void Matrix::Reset(int numRows, int numCols)
 {
-    // your implementation here
+    if (numRows < 0 || numCols < 0) 
+    {
+        throw out_of_range("");
+    }
+    if (numRows == 0 || numCols == 0) 
+    {
+        x = 0;
+        y = 0;
+        Z.clear();
+    }
+    else 
+    {
+        x = numRows;
+        y = numCols;
+        Z.assign(x, vector<int>(y, 0));
+    }
 }
 
 int& Matrix::At(int row, int col)
 {
-    // your implementation here
+    if (row < 0 || row >= x || col < 0 || col >= y) 
+    {
+        throw out_of_range("");
+    }
+    return Z[row][col];
 }
 
 const int& Matrix::At(int row, int col) const
 {
-    // your implementation here
+    if (row < 0 || row >= x || col < 0 || col >= y)
+    {
+        throw out_of_range("");
+    }
+    return Z[row][col];
 }
 
 int Matrix::GetRows() const
 {
-    // your implementation here
+    return x;
 }
 
 int Matrix::GetCols() const
 {
-    // your implementation here
+    return y;
 }
 
 bool Matrix::operator==(const Matrix& m2)
 {
-    // your implementation here
+    if (x!=m2.GetRows() || y != m2.GetCols())
+    {
+        return false;
+    }
+    else
+    {
+        int i = 0, j=0;
+        bool b = true;
+        for (i; i < x; ++i)
+        {
+            for (j; j < y; ++j)
+            {
+                if (Z[i][j] != m2.At(i, j))
+                {
+                    b = false;
+                    break;
+                }
+            }
+        }   
+        return b;
+    }
 }
 
 bool Matrix::operator!=(const Matrix& m2)
 {
-    // your implementation here
+    return not(this->operator==(m2));
 }
 
 Matrix Matrix::operator+(const Matrix& m2)
 {
-    // your implementation here
+    if (x == m2.GetRows() && y == m2.GetCols())
+    {
+        int i = 0, j = 0;
+        for (i; i < x; ++i)
+        {
+            for (j; j < y; ++j)
+            {
+                Z[i][j] += m2.At(i, j);
+            }
+        };
+    }
+    else
+    {
+        throw invalid_argument("");
+    }
 }
