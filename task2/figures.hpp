5ab7f22b1d3a09278acc006e16aea50326ca2a52
SergeyyVorobeyy <Sergeysergeev2004@gmail.com>
2024-12-28 14:55:14 +0500

    Update figures.hpp

--- task2/figures.hpp
+++ task2/figures.hpp
@@ -19,23 +19,64 @@ public:
 
 class Rect : public Figure
 {
+public:
+    Rect(double height, double width);
+
+    virtual FigureType Type() const override;
+    virtual double Perimeter() const override;
+    virtual double Area() const override;
+
+private:
+    double height_;
+    double width_;
 };
 
 class Triangle : public Figure
 {
+public:
+    Triangle(double a, double b, double c);
+
+    static bool IsValidTriangle(double a, double b, double c);
+
+    virtual FigureType Type() const override;
+    virtual double Perimeter() const override;
+    virtual double Area() const override;
+
+private:
+    double a_;
+    double b_;
+    double c_;
 };
 
 class Circle : public Figure
 {
+public:
+    Circle(double radius);
+
+    virtual FigureType Type() const override;
+    virtual double Perimeter() const override;
+    virtual double Area() const override;
+
+private:
+    double radius_;
 };
 
 std::unique_ptr<Figure> make_figure(FigureType type, double a, double b = 0, double c = 0);
 
 class WrongTriangle : public std::invalid_argument
 {
+public:
+    WrongTriangle(std::string msg);
+
+private:
+    std::string _exceptionMsg;
 };
 
 class LessThanZeroParam : public std::invalid_argument
 {
-};
+public:
+    LessThanZeroParam(std::string msg);
 
+private:
+    std::string _exceptionMsg;
+};
