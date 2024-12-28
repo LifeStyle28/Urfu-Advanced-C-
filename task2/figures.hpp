c5c5b297db771e43b18962d0b2d0fa072961ee9d
MAXmvp228 <maksimbobylev2005@gmail.com>
2024-12-29 02:47:06 +0500

    Update figures.hpp

--- task2/figures.hpp
+++ task2/figures.hpp
@@ -19,14 +19,35 @@ public:
 
 class Rect : public Figure
 {
+private:
+    double l,h;
+public:
+    Rect(double l, double h);
+    FigureType Type() const;
+    double Perimeter() const;
+    double Area() const;
 };
 
 class Triangle : public Figure
 {
+private:
+    double a,b,c;
+public:
+    Rect(double a, double b, double c);
+    FigureType Type() const;
+    double Perimeter() const;
+    double Area() const;
 };
 
 class Circle : public Figure
 {
+private:
+    double r;
+public:
+    Rect(double r);
+    FigureType Type() const;
+    double Perimeter() const;
+    double Area() const;
 };
 
 std::unique_ptr<Figure> make_figure(FigureType type, double a, double b = 0, double c = 0);
