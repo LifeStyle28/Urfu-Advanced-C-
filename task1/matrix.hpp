6d582b1ba8f0556be4991620931119edde828afe
Lampa-lab <denisgaltsov64@gmail.com>
2024-12-30 01:55:18 +0500

    matrix.hpp

--- task1/matrix.hpp
+++ task1/matrix.hpp
@@ -11,8 +11,26 @@ private:
     int cols;
     std::vector<std::vector<int>> mat;
 public:
-    Matrix() = default;
-    Matrix(int numRows, int numCols);
+     Matrix() : num_rows(0), num_cols(0) {}
+    explicit Matrix(int rows, int cols);
+    
+    Matrix(const Matrix& other) = default;
+    Matrix& operator=(const Matrix& other) = default;
+    
+    Matrix(Matrix&& other) noexcept : num_rows(other.num_rows), num_cols(other.num_cols), data(std::move(other.data)) {
+        other.num_rows = 0;
+        other.num_cols = 0;
+    }
+    Matrix& operator=(Matrix&& other) noexcept {
+        if (this != &other) {
+            num_rows = other.num_rows;
+            num_cols = other.num_cols;
+            data = std::move(other.data);
+            other.num_rows = 0;
+            other.num_cols = 0;
+        }
+        return *this;
+    }
 
     void Reset(int numRows, int numCols);
     int At(int row, int col) const;
@@ -20,8 +38,7 @@ public:
     int GetRows() const;
     int GetCols() const;
 
-    bool operator==(const Matrix& m2);
-    bool operator!=(const Matrix& m2);
-    Matrix operator+(const Matrix& m2);
-
+    friend bool operator==(const Matrix& lhs, const Matrix& rhs);
+    friend bool operator!=(const Matrix& lhs, const Matrix& rhs);
+    friend Matrix operator+(const Matrix& lhs, const Matrix& rhs);
 };
