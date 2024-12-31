8c01c311d5062d8fe2028f58071e04fbafc20ed1
ADOBblU <misha.lazutkin@list.ru>
2024-12-31 11:19:30 +0500

    matrix.hpp

--- task1/matrix.hpp
+++ task1/matrix.hpp
@@ -1,5 +1,6 @@
 #pragma once
-
+#include <vector>
+#include <stdexcept>
 class Matrix
 {
 public:
@@ -15,4 +16,9 @@ public:
     bool operator==(const Matrix& m2);
     bool operator!=(const Matrix& m2);
     Matrix operator+(const Matrix& m2);
+
+private:
+    std::vector<std::vector<int>> data;
+    int numRows = 0;
+    int numCols = 0;
 };
