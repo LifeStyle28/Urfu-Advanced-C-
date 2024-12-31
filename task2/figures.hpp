85d1974a24c270e7e4c2948a52532782db1f6f7b
sfgfrevnjdsfkvdfkvbjhefve <semencharonovich22.8@gmail.com>
2024-12-31 19:06:47 +0300

    Update figures.hpp

--- task2/figures.hpp
+++ task2/figures.hpp
@@ -2,40 +2,65 @@
 
 #include <memory>
 #include <stdexcept>
+#include <cmath>
 
-enum class FigureType
-{
-    TRIANGLE,
-    CIRCLE,
-    RECTANGLE,
+enum class ShapeType {
+    Triangle,
+    Circle,
+    Rectangle
 };
 
-class Figure {
+class Shape {
 public:
-    virtual FigureType Type() const = 0;
-    virtual double Perimeter() const = 0;
-    virtual double Area() const = 0;
+    virtual ~Shape() = default;
+
+    virtual ShapeType GetType() const = 0;
+    virtual double GetPerimeter() const = 0;
+    virtual double GetArea() const = 0;
 };
 
-class Rect : public Figure
-{
+class Rectangle : public Shape {
+private:
+    double width;
+    double height;
+
+public:
+    Rectangle(double width, double height);
+    ShapeType GetType() const override;
+    double GetPerimeter() const override;
+    double GetArea() const override;
 };
 
-class Triangle : public Figure
-{
+class Triangle : public Shape {
+private:
+    double sideA, sideB, sideC;
+
+public:
+    Triangle(double a, double b, double c);
+    ShapeType GetType() const override;
+    double GetPerimeter() const override;
+    double GetArea() const override;
 };
 
-class Circle : public Figure
-{
+class Circle : public Shape {
+private:
+    double radius;
+
+public:
+    Circle(double radius);
+    ShapeType GetType() const override;
+    double GetPerimeter() const override;
+    double GetArea() const override;
 };
 
-std::unique_ptr<Figure> make_figure(FigureType type, double a, double b = 0, double c = 0);
+std::unique_ptr<Shape> CreateShape(ShapeType type, double a, double b = 0, double c = 0);
 
-class WrongTriangle : public std::invalid_argument
-{
+class InvalidTriangleException : public std::invalid_argument {
+public:
+    InvalidTriangleException(const char* msg) : std::invalid_argument(msg) {}
 };
 
-class LessThanZeroParam : public std::invalid_argument
-{
+class NegativeParameterException : public std::invalid_argument {
+public:
+    NegativeParameterException(const char* msg) : std::invalid_argument(msg) {}
 };
-
