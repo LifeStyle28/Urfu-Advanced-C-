d45c465918710cbfeb3080b14459222de9b140aa
zxcPodpivas <147530959+zxcPodpivas@users.noreply.github.com>
2025-01-02 22:52:47 +0500

    Update figures.hpp

--- task2/figures.hpp
+++ task2/figures.hpp
@@ -1,8 +1,9 @@
 #pragma once
-
+#include <cmath>
 #include <memory>
 #include <stdexcept>
-
+#include <numbers>
+static constexpr double pi = std::numbers::pi;
 enum class FigureType
 {
     TRIANGLE,
@@ -13,29 +14,56 @@ enum class FigureType
 class Figure {
 public:
     virtual FigureType Type() const = 0;
+    virtual ~Figure() = default; 
     virtual double Perimeter() const = 0;
     virtual double Area() const = 0;
 };
 
 class Rect : public Figure
 {
+private:
+    double width;
+    double height;
+public:
+    Rect(double width, double height);
+    FigureType Type() const override;
+    double Perimeter() const override;
+    double Area() const override;
 };
 
 class Triangle : public Figure
 {
+private:
+    double a, b, c;
+public:
+    Triangle(double side_a, double side_b, double side_c);
+    FigureType Type() const override;
+    double Perimeter() const override;
+    double Area() const override;
 };
 
 class Circle : public Figure
 {
+public:
+    Circle(double rad);
+    FigureType Type() const override;
+    double Perimeter() const override;
+    double Area() const override;
+private:
+    double rad;
 };
 
 std::unique_ptr<Figure> make_figure(FigureType type, double a, double b = 0, double c = 0);
 
 class WrongTriangle : public std::invalid_argument
 {
+public:
+    using std::invalid_argument::invalid_argument;
 };
 
 class LessThanZeroParam : public std::invalid_argument
 {
+public:
+    using std::invalid_argument::invalid_argument;
 };
 
