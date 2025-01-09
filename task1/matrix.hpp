a4abebdff9e19e3ec1cfc6c781aa9e5efa4d741d
Kirill-Tsypushtanov <den270380@gmail.com>
2025-01-09 10:54:33 +0500

    Update matrix.hpp

--- task1/matrix.hpp
+++ task1/matrix.hpp
@@ -8,13 +8,13 @@ class Matrix
 public:
     Matrix() = default;
     Matrix(int numRows, int numCols);
+
     void Reset(int numRows, int numCols);
     int& At(int row, int col);
     const int& At(int row, int col) const;
     int GetRows() const;
     int GetCols() const;
 
- 
     bool operator==(const Matrix& m2) const;
     bool operator!=(const Matrix& m2) const;
     Matrix operator+(const Matrix& m2) const;
@@ -23,3 +23,4 @@ private:
     int rows = 0;
     int cols = 0;
     std::vector<std::vector<int>> data;
+};
