3905c4152f46a19334fce2af15be0266a64fd687
Kirill-Tsypushtanov <den270380@gmail.com>
2025-01-09 11:50:55 +0500

    Update circular_queue.hpp

--- task3/circular_queue.hpp
+++ task3/circular_queue.hpp
@@ -1,31 +1,20 @@
 #pragma once
-
 #include <cstddef>
 
 class CircularQueue {
-private:   
+public:
+    CircularQueue(size_t size); // создать очередь с определенным размером буффера
+    bool Push(int value); // добавить значение в конец очереди (false, если очередь заполнена)
+    bool Pop(); // удалить значение из начала очереди (false, если очередь пустая)
+    int Front() const; // получить значение из начала очереди (-1, если очередь пустая)
+    int Back() const; // получить значение из конца очереди (-1, если очередь пустая)
+    bool Empty() const; // проверить пустая ли очередь
+    bool Full() const; // проверить заполнена ли очередь
+
+private:
     int* buffer; 
     size_t capacity;
     size_t front;
     size_t rear;
     size_t count;
-
-public:
-    explicit CircularQueue(size_t size); 
-    ~CircularQueue(); 
-
-    
-    CircularQueue(const CircularQueue&) = delete;
-    CircularQueue& operator=(const CircularQueue&) = delete;
-
-   
-    CircularQueue(CircularQueue&& other) noexcept;
-    CircularQueue& operator=(CircularQueue&& other) noexcept;
-
-    bool Push(int value); 
-    bool Pop(); 
-    int Front() const; 
-    int Back() const;
-    bool Empty() const; 
-    bool Full() const; 
 };
