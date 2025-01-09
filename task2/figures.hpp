b3fd6c66911e3642932dcba9fde9c42f0e3c9321
Kirill-Tsypushtanov <den270380@gmail.com>
2025-01-09 12:22:05 +0500

    Update figures.hpp

--- task2/figures.hpp
+++ task2/figures.hpp
@@ -2,51 +2,48 @@
 #include <memory>
 #include <stdexcept>
 static constexpr double PI = 3.14;
-
-enum class FigureType {
+enum class FigureType 
+{
     TRIANGLE,
     CIRCLE,
     RECTANGLE,
 };
-
-class Figure {
+class Figure 
+{
 public:
     virtual FigureType Type() const = 0;
     virtual double Perimeter() const = 0;
     virtual double Area() const = 0;
     virtual ~Figure() = default;
 };
-
-class Rect : public Figure {
+class Rect : public Figure
+{
 public:
     Rect(double width, double height);
     FigureType Type() const override;
     double Perimeter() const override;
     double Area() const override;
-
 private:
     double width;
     double height;
 };
-
-class Triangle : public Figure {
+class Triangle : public Figure 
+{
 public:
     Triangle(double a, double b, double c);
     FigureType Type() const override;
     double Perimeter() const override;
     double Area() const override;
-
 private:
     double a, b, c;
 };
-
-class Circle : public Figure {
+class Circle : public Figure 
+{
 public:
     Circle(double radius);
     FigureType Type() const override;
     double Perimeter() const override;
     double Area() const override;
-
 private:
     double radius;
 };
