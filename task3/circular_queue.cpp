46a0ea474d7560e2bb7c558be7605ee0fa003548
Kirill-Tsypushtanov <den270380@gmail.com>
2025-01-09 12:29:43 +0500

    Update circular_queue.cpp

--- task3/circular_queue.cpp
+++ task3/circular_queue.cpp
@@ -1,74 +1,33 @@
 #include "circular_queue.hpp"
 #include <stdexcept>
-#include <utility>
 
-CircularQueue::CircularQueue(size_t size) 
-    : capacity(size), front(0), rear(0), count(0) {
-    if (size == 0) {
-        throw std::invalid_argument("Size of the queue must be greater than 0");
-    }
-    buffer = new int[size];
-}
-
-CircularQueue::~CircularQueue() {
-    delete[] buffer;
-}
-
-CircularQueue::CircularQueue(CircularQueue&& other) noexcept 
-    : buffer(nullptr), capacity(0), front(0), rear(0), count(0) {
-    *this = std::move(other);
-}
-
-CircularQueue& CircularQueue::operator=(CircularQueue&& other) noexcept {
-    if (this != &other) {
-        delete[] buffer;
-
-        buffer = other.buffer;
-        capacity = other.capacity;
-        front = other.front;
-        rear = other.rear;
-        count = other.count;
-
-        other.buffer = nullptr;
-        other.capacity = 0;
-        other.front = 0;
-        other.rear = 0;
-        other.count = 0;
-    }
-    return *this;
-}
+CircularQueue::CircularQueue(size_t size) : buffer(new int[size]), capacity(size), front(0), rear(0), count(0) {}
 
 bool CircularQueue::Push(int value) {
-    if (Full()) {
+    if (count == capacity) {
         return false;
     }
     buffer[rear] = value;
     rear = (rear + 1) % capacity;
-    count++;
+    ++count;
     return true;
 }
 
 bool CircularQueue::Pop() {
-    if (Empty()) {
+    if (count == 0) {
         return false;
     }
     front = (front + 1) % capacity;
-    count--;
+    --count;
     return true;
 }
 
 int CircularQueue::Front() const {
-    if (Empty()) {
-        return -1;
-    }
-    return buffer[front];
+    return count == 0 ? -1 : buffer[front];
 }
 
 int CircularQueue::Back() const {
-    if (Empty()) {
-        return -1;
-    }
-    return buffer[(rear == 0 ? capacity : rear) - 1];
+    return count == 0 ? -1 : buffer[(rear - 1 + capacity) % capacity];
 }
 
 bool CircularQueue::Empty() const {
