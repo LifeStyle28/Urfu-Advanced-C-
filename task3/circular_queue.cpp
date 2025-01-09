8f24696952971a80406fe19be5ecf71d511731fe
Haitle <mrtifes@gmail.com>
2025-01-09 23:15:03 +0500

    Update circular_queue.cpp

--- task3/circular_queue.cpp
+++ task3/circular_queue.cpp
@@ -1,36 +1,45 @@
 #include "circular_queue.hpp"
 
 CircularQueue::CircularQueue(size_t size)
-{
-	// your implementation here
+    : buffer_(size), head_(0), tail_(0), capacity_(size), count_(0) {}
+
+bool CircularQueue::Push(int value) {
+    if (Full()) {
+        return false;
+    }
+    buffer_[tail_] = value;
+    tail_ = (tail_ + 1) % capacity_;
+    ++count_;
+    return true;
 }
 
-bool CircularQueue::Push(int value)
-{
-    // your implementation here
+bool CircularQueue::Pop() {
+    if (Empty()) {
+        return false;
+    }
+    head_ = (head_ + 1) % capacity_;
+    --count_;
+    return true;
 }
 
-bool CircularQueue::Pop()
-{
-    // your implementation here
+int CircularQueue::Front() const {
+    if (Empty()) {
+        return -1;
+    }
+    return buffer_[head_];
 }
 
-int CircularQueue::Front() const
-{
-    // your implementation here
+int CircularQueue::Back() const {
+    if (Empty()) {
+        return -1;
+    }
+    return buffer_[(tail_ + capacity_ - 1) % capacity_];
 }
 
-int CircularQueue::Back() const
-{
-    // your implementation here
+bool CircularQueue::Empty() const {
+    return count_ == 0;
 }
 
-bool CircularQueue::Empty() const
-{
-    // your implementation here
-}
-
-bool CircularQueue::Full() const
-{
-    // your implementation here
+bool CircularQueue::Full() const {
+    return count_ == capacity_;
 }
